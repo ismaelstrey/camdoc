e88a318e0308ea5fd44fa3cb5c1efa26a4a1a667
ismael strey pereira <ismaelstrey@hotmail.com>
2025-01-27 16:52:16 -0300

    first commit

---
+++ src/app/components/cam/index.tsx
@@ -0,0 +1,61 @@
+'use client'
+import React, { useRef, useState } from 'react';
+
+export default function Cam() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [images, setImages] = useState<string[]>([]);
+
+    const startCamera = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+        }
+    };
+
+    const takePhoto = () => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        if (videoRef.current) {
+            canvas.width = videoRef.current.videoWidth;
+            canvas.height = videoRef.current.videoHeight;
+            context?.drawImage(videoRef.current, 0, 0);
+            if (images.length < 4) {
+                setImages([...images, canvas.toDataURL('image/png')]);
+            } else {
+                alert('Você já tirou 4 fotos!');
+            }
+        }
+    };
+
+    const deletePhoto = (index: number) => {
+        setImages(images.filter((_, i) => i !== index));
+    };
+
+    const uploadPhoto = () => {
+        images.forEach(image => {
+            console.log('Imagem para upload:', image);
+        });
+    };
+
+    React.useEffect(() => {
+        startCamera();
+    }, []);
+
+    return (
+        <div className='flex flex-col items-center content-center mt-8'>
+            <div className='flex fixed top-4 left-4 p-4 mt-8 gap-8'>
+                <button onClick={takePhoto}>Tirar Foto</button>
+                <button onClick={uploadPhoto}>Fazer Upload</button>
+            </div>
+            <video className='mt-8' ref={videoRef} autoPlay />
+
+            <div className='flex flex-col w-80'>
+                {images.map((image, index) => (
+                    <div key={index}>
+                        <img src={image} alt={`Foto ${index + 1}`} />
+                        <button onClick={() => deletePhoto(index)}>Apagar Foto</button>
+                    </div>
+                ))}</div>
+        </div>
+    );
+}
